beb7d19f38bba9a196e94b5a9545e340379b5b98
Tabish Sharma <tabishsharma01236@gmail.com>
2024-07-25 11:47:57 +0530

    Add node_modules to .gitignore

--- src/Contexts/firebase.js
+++ src/Contexts/firebase.js
@@ -3,15 +3,18 @@ import "firebase/compat/auth";
 import { getFirestore } from "firebase/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAcHuSnmxym6-q6g-9esIjeBDuVw3DFp1M",
-  authDomain: "lms-portal-1f2dc.firebaseapp.com",
-  projectId: "lms-portal-1f2dc",
-  storageBucket: "lms-portal-1f2dc.appspot.com",
-  messagingSenderId: "471940190175",
-  appId: "1:471940190175:web:284c14b69b13ffdd38be6a"
+  apiKey: "AIzaSyASI-5F2_t7yDEFXxN_0b1MqDM8VIVO92s",
+  authDomain: "lmsp-701e7.firebaseapp.com",
+  projectId: "lmsp-701e7",
+  storageBucket: "lmsp-701e7.appspot.com",
+  messagingSenderId: "140727697051",
+  appId: "1:140727697051:web:31d7b278ec39bbb7d7103e",
+  measurementId: "G-XG58JQ12SK",
 });
+
 
 const db = getFirestore(app);
 const auth = firebase.auth();
+
 
 export { db, auth };
